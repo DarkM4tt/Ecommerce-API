8513f0e10bdfe3a2e3f0ee983021d982990f1c34
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-08-15 13:22:27 +0530

    Added mutations.

--- server.js
+++ server.js
@@ -5,22 +5,11 @@ const { loadFilesSync } = require('@graphql-tools/load-files')
 const { makeExecutableSchema } = require('@graphql-tools/schema')
 
 const typesArray = loadFilesSync(path.join(__dirname, '**/*.graphql'))
+const resolversArray = loadFilesSync(path.join(__dirname, '**/*.resolvers.js'))
 
 const schema = makeExecutableSchema({
   typeDefs: typesArray,
-  resolvers: {
-    Query: {
-      products: async (parent) => {
-        console.log('Getting the products...')
-        const product = await Promise.resolve(parent.products)
-        return product
-      },
-      orders: (parent) => {
-        console.log('Getting Orders...')
-        return parent.orders
-      },
-    },
-  },
+  resolvers: resolversArray,
 })
 
 const root = {
